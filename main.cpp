94fded0dff95bc9f77cbbd589cc9d38194b3a3cb
Matthew <matthew.budde.mb@gmail.com>
2018-06-03 14:47:55 -0400

    First commit - just FTDI header file and initial bit of main

---
+++ main.cpp
@@ -0,0 +1,14 @@
+/*
+ *
+ *   BME280 Logger Software
+ *   Author: Matthew Budde - 2018
+ *
+ *   This program interfaces with the BME280 Logger board developed by
+ *   Matthew Budde to provide a user interface to log and monitor temperature,
+ *   pressure, and humidity.
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "FTD2XX.h"
